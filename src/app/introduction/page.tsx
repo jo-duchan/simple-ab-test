741cd33c80064e9f1bdc079f749e036b4d328146
Duchan <jo_duchan@icloud.com>
2024-12-14 00:42:25 +0900

    chore: Introduction Page 임시 생성

---
+++ src/app/introduction/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./page.module.scss";
+import FigmaPrototype from "@/shared/components/FigmaPrototype";
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <h3>Introduction Page</h3>
+      <FigmaPrototype
+        url="https://www.figma.com/proto/iIwEmaTF9xhYmyNdgqjL1b/test11?node-id=7-3&node-type=frame&t=Vvt0RLmYtybsltha-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=7%3A3"
+        lastNodeId="7-9"
+      />
+    </div>
+  );
+}

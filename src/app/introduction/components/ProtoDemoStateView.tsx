8fd5a0c310772fa2b4bea60861611cc602aec6c3
Duchan <jo_duchan@icloud.com>
2024-12-14 23:48:20 +0900

    chore: introduction page UI 관련 작업

---
+++ src/app/introduction/components/ProtoDemoStateView.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./ProtoDemoStateView.module.scss";
+
+export default function ProtoDemoStateView() {
+  return (
+    <div className={styles.container}>
+      <div>some state...</div>
+    </div>
+  );
+}

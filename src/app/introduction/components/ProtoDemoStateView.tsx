25e6391f144f5b525c25e710d3277524c8db2575
Duchan <jo_duchan@icloud.com>
2024-12-15 01:31:44 +0900

    beta 0.1.1

--- src/app/introduction/components/ProtoDemoStateView.tsx
+++ src/app/introduction/components/ProtoDemoStateView.tsx
@@ -37,7 +37,7 @@ export default function ProtoDemoStateView() {
         <h3 className={styles.totalTimeSpent}>
           목적 달성 시간:{" "}
           {Object.entries(data.timeSpent)
-            .reduce((acc, [key, time]) => (acc += time), 0)
+            .reduce((acc, cur) => (acc += cur[1]), 0)
             .toString()}
           ms
         </h3>
